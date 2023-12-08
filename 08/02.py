08d27a3d0950fa7278aedd99dd00808e4d998234
sloproo <3440817+sloproo@users.noreply.github.com>
2023-12-08 10:19:07 +0200

    webbi-IDELLÄ editoitu, mut ratkaisussa tuntuu kestävän

--- 08/02.py
+++ 08/02.py
@@ -5,29 +5,29 @@ with open ("input.txt") as f:
     suunnat = f.readline().strip()
     f.readline()
     for r in f:
-        paikat = re.findall("[A-Z]{3}", r)
+        paikat = re.findall("[0-9A-Z]{3}", r)
+        print(paikat)
         haarat[paikat[0]] = (paikat[1], paikat[2])
-
-print(suunnat)
-print(haarat)
-pass
 
 askeleita = 0
 maalissa = False
-oma_paikka = "AAA"
+kummitusten_paikat = [alku for alku in haarat.keys() if alku[-1] == "A"]
 
 while not maalissa:
     for suunta in suunnat:
-        print(oma_paikka)
         askeleita += 1
-        if suunta == "L":
-            oma_paikka = haarat[oma_paikka][0]
-            print(f"Suunta on {suunta}, mennään paikkaan {oma_paikka}")
-        elif suunta == "R":
-            oma_paikka = haarat[oma_paikka][1]
-
-        if oma_paikka == "ZZZ":
+        for i in range(len(kummitusten_paikat)):
+            if suunta == "L":
+                kummitusten_paikat[i] = haarat[kummitusten_paikat[i]][0]
+                # print(f"Suunta on {suunta}, mennään paikkaan {kummitusten_paikat[i]}")
+            elif suunta == "R":
+                kummitusten_paikat[i] = haarat[kummitusten_paikat[i]][1]
+        
+        for kummitus in kummitusten_paikat:
+            if kummitus[-1] != "Z":
+                break
+        else:
             maalissa = True
             break
-
+        
 print(askeleita)
